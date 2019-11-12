1a7a558706b805ffde987146a6c1738a2a2aecb1
eiwanaga <eiwanaga@hawaii.edu>
2019-11-12 11:19:12 -1000

    deleted code test folders

--- test1-sassybase/js/script.js
+++
@@ -1,7 +0,0 @@
-const mysitenav = document.querySelector('.site-header .site-nav');
-const mymenubutton = document.querySelector('.menu-button');
-
-mymenubutton.onclick = function () {
-    console.log("clicked!");
-    mysitenav.classList.toggle('active');
-}
